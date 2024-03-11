0fa0a2f5387f1d83234bb969a24ef6e84d57a01b
p0lluxstar <web-registra@yandex.ru>
2024-03-11 16:17:47 +0300

    feat: add env

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { startSwagger } from './swagger/swaggerConfig';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT || '4000';
 
   await startSwagger(app);
 
-  await app.listen(4000, () =>
-    console.log(`\x1b[35mApplication is running on port: 4000\x1b[0m`),
+  await app.listen(PORT, () =>
+    console.log(
+      `\x1b[35mApplication is currently running on port: ${PORT}\x1b[0m`,
+    ),
   );
 }
 bootstrap();
